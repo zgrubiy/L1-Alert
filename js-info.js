ae188002053b97d6c66a5f0af34183d2ce37d2f0
zgrubiy <52798808+zgrubiy@users.noreply.github.com>
2022-01-11 15:47:37 +0300

    Add files via upload

---
+++ js-info.js
@@ -0,0 +1,7 @@
+console.log('Начало общих вопросов');
+alert("Язык программирования JavaScript разработал Брендан Эйх");
+alert("JavaScript был разработан за десять дней");
+alert("JavaScript появился 4 декабря 1995 года");
+alert("JavaScript это не Java");
+alert("Cпецификация JavaScript называется 'ECMAScript' (стандарт ECMA-262)");
+console.log('Конец общих вопросов');
